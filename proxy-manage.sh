18ab28807543ccd46a506706596247002ae7200f
kansei <kansei@kansei.local>
2024-12-28 17:27:12 +0900

    update

--- proxy-manage.sh
+++ proxy-manage.sh
@@ -1,22 +1,29 @@
 ## !/bin/bash
 
-
 main () {
   local PROXY_ID='i-0db77541139990ecd'
   local DNS_NAME="$(aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | select(.InstanceId==\"${PROXY_ID}\") | .PublicDnsName")"
 
   if [ ! -z "${DNS_NAME}" ]; then
-    echo "stop"
     aws ec2 stop-instances --instance-ids "${PROXY_ID}" >> /dev/null
-    sleep 120
+
+    while : ;
+    do
+      sleep 5
+      local state="$(aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | select(.InstanceId==\"${PROXY_ID}\") | .State.Name")"
+
+      if [ "${state}" == 'stopped' ]; then
+        break
+      fi
+    done
   fi
 
   DNS_NAME=""
   aws ec2 start-instances --instance-ids "${PROXY_ID}" >> /dev/null
-  
+
   while : ;
   do
-    sleep 1
+    sleep 2
     DNS_NAME="$(aws ec2 describe-instances | jq -r '.Reservations[].Instances[].PublicDnsName')"
 
     if [ -z "${DNS_NAME}" ]; then
